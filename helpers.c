53664774cd426ec9e5d3667363cda81babefd283
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-03-08 16:33:06 +0500

    Implement sepia().

--- helpers.c
+++ helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <stdio.h>
 #include <math.h>
 
 // Convert image to grayscale
@@ -29,6 +30,48 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    /* Algorithm for applying Sepian tone to a pixel
+    sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+    sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+    sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue */
+
+    BYTE oRed;
+    WORD sRed;
+
+    BYTE oGreen;
+    WORD sGreen;
+
+    BYTE oBlue;
+    WORD sBlue;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Aliases for original colors
+            oRed = image[i][j].rgbtRed;
+            oGreen = image[i][j].rgbtGreen;
+            oBlue = image[i][j].rgbtBlue;
+
+            // Obtaining Sepian tone of original colors
+            sRed = round((0.393 * oRed) + (0.769 * oGreen) + (0.189 * oBlue));
+            sGreen = round((0.349 * oRed) + (0.686 * oGreen) + (0.168 * oBlue));
+            sBlue = round((0.272 * oRed) + (0.534 * oGreen) + (0.131 * oBlue));
+            
+            // Rounding Sepian tone colors to nearest integer in domain of [0,255]
+            sBlue = (sBlue > 255)? 255: sBlue;
+            sGreen = (sGreen > 255)? 255: sGreen;
+            sRed = (sRed > 255)? 255: sRed;       
+
+            // Creating a pixel with sepian tone of the origianl pixel
+            RGBTRIPLE sepianPixel = {sBlue, sGreen, sRed};
+            
+            // Applying Sepian tone to original image's pixel
+            image[i][j] = sepianPixel;
+        }
+    }
+
     return;
 }
 
