458001bf5ee6a31b047c40902a6510e75454ed2b
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-03-08 14:50:37 +0500

    Implement grayscale(). Change compiler to clang

--- helpers.c
+++ helpers.c
@@ -1,8 +1,27 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Shades of grey can be obtained by setting equal values for all three colors
+
+    // Stores the obtained shade of pixel
+    BYTE shade;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            shade = 
+            round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+
+            image[i][j].rgbtRed = shade;
+            image[i][j].rgbtGreen = shade;
+            image[i][j].rgbtBlue = shade;
+        }
+    }
+
     return;
 }
 
